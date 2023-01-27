59d25ef8d53372bf6aa0f07bc3b0e017df2795b7
manu <manoj.pk6695@gmail.com>
2023-01-27 17:37:41 +0530

    added provider

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my_instance"{
-    ami : "ami-06984ea821ac0a879"
-    type : "t2-micro"
-    Count : 2
-    Tags : {
-        Name : my_insta
+    ami = "ami-06984ea821ac0a879"
+    type = "t2-micro"
+    Count = 2
+    Tags = {
+        Name = my_insta
     }
 }
